6210e3c7137f301aaee850f63dcf88b70025807b
Manny Pissanos <mpissano@gmail.com>
2018-09-27 17:49:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,16 @@ def name
   @name
 end
   
-def name=(dog)
-  @name = dog
+def name=(dog_name)
+  @name = dog_name
 end
+ 
+def breed
+    @breed
+end
+
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+
 end
